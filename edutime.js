b0482df36989f66f0818e6d9daa9749e05bc0b1e
el983 <elpislearningconsultantsllp88@gmail.com>
2025-06-25 16:57:19 +0530

    Add files via upload

--- edutime.js
+++ edutime.js
@@ -4,7 +4,60 @@ const slides = [
     content2:"HERALD GROUP",
     className:"slide-style-1"
   },
+  {
+    content1:"Director - Training & Extended Learning",
+    content2:"TECHNO INDIA GROUP",
+    className:"slide-style-1",
+  },
+  {
+    content1:"Director - Professional Studies",
+    content2:"ADAMAS UNIVERSITY",
+    className:"slide-style-1",
+  },
+  {
+    content1:"Training Manager",
+    content2:"AMAZON Global",
+    className:"slide-style-1",
+  },
+  {
+    content1:"Associate Professor & HOD ",
+    content2:" RAMOJI KRIAN UNIVERSITY",
+    className:"slide-style-1",
+  },
+  {
+    content1:"Head - Learning & Development",
+    content2:"THE MINDSHIFT",
+    className:"slide-style-1",
+  },
+  {
+    content1:"Senior Manager- Learning & Development",
+    content2:"CERNER",
+    className:"slide-style-1",
+  },
+  {
+    content1:"Assistant Training Manager",
+    content2:"Anthelio Business Technologies",
+    className:"slide-style-1",
+  },
+  {
+    content1:"Communication Trainer ",
+    content2:"PINNACLE ASSOCIATES",
+    className:"slide-style-1",
+  },
+  {
+    content1:"Team Leader- Communication Training",
+    content2:"INFOSYS",
+    className:"slide-style-1",
+  },
+  {
+    content1:"Lead Trainer",
+    content2:"IBM",
+    className:"slide-style-1",
+  },
   
+
+  
+
   
   {
    
